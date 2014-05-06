84400a74faf44a21d7042d069dc5230b14aa9b8f
André Schneider <hello@andreschneider.me>
2014-05-06 21:07:25 +0200

    Initial commit.

---
+++ Popping/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Popping
+//
+//  Created by André Schneider on 06.05.14.
+//  Copyright (c) 2014 André Schneider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
