9b5e6af1119ecf9d8ef00e8990a78e5b7c8d749d
André Schneider <hello@andreschneider.me>
2014-05-17 12:54:29 +0200

    add a titleLabel animation as the button action.

--- Popping/PaperButtonViewController.m
+++ Popping/PaperButtonViewController.m
@@ -8,6 +8,14 @@
 
 #import "PaperButtonViewController.h"
 #import "PaperButton.h"
+#import <POP/POP.h>
+
+@interface PaperButtonViewController()
+@property(nonatomic) UILabel *titleLabel;
+- (void)addControls;
+- (void)animateTitleLabel:(id)sender;
+- (void)setTitleLabel;
+@end
 
 @implementation PaperButtonViewController
 
@@ -17,10 +25,55 @@
     self.title = @"Paper Button Animation";
     self.view.backgroundColor = [UIColor whiteColor];
 
+    [self addControls];
+}
+
+# pragma mark - Private Instance methods
+
+- (void)addControls
+{
     CGPoint origin = CGPointMake(CGRectGetWidth(self.view.frame) - 56.f, 72.f);
 
     PaperButton *button = [PaperButton buttonWithOrigin:origin];
+    [button addTarget:self action:@selector(animateTitleLabel:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
+
+    CGFloat titlePadding = 20.f;
+    CGRect titleFrame = CGRectMake(titlePadding,
+                                   origin.y,
+                                   CGRectGetMinX(button.frame) - titlePadding*2,
+                                   CGRectGetHeight(button.bounds));
+
+    self.titleLabel = [[UILabel alloc] initWithFrame:titleFrame];
+    self.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:22.f];
+    [self setTitleLabel];
+    [self.view addSubview:self.titleLabel];
+}
+
+- (void)animateTitleLabel:(id)sender
+{
+    POPSpringAnimation *onscreenAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionX];
+    onscreenAnimation.toValue = @(self.titleLabel.center.x);
+    onscreenAnimation.springBounciness = 10.f;
+
+    POPBasicAnimation *offscreenAnimation = [POPBasicAnimation easeInAnimation];
+    offscreenAnimation.property = [POPAnimatableProperty propertyWithName:kPOPLayerPositionX];
+    offscreenAnimation.toValue = @(-self.titleLabel.layer.position.y);
+    offscreenAnimation.duration = 0.2f;
+    [offscreenAnimation setCompletionBlock:^(POPAnimation *anim, BOOL finished) {
+        [self setTitleLabel];
+        [self.titleLabel.layer pop_addAnimation:onscreenAnimation forKey:@"onscreenAnimation"];
+    }];
+    [self.titleLabel.layer pop_addAnimation:offscreenAnimation forKey:@"offscreenAnimation"];
+}
+
+- (void)setTitleLabel
+{
+    NSString *title = @"List";
+    if ([self.titleLabel.text isEqualToString:title]) {
+        title = @"Menu";
+    }
+    self.titleLabel.text = title;
 }
 
 @end
