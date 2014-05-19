002f81de9957843572f33d96575f6c78bda7ee65
André Schneider <hello@andreschneider.me>
2014-05-19 22:53:58 +0200

    Add a bar button item.

--- Popping/ConstraintsViewController.m
+++ Popping/ConstraintsViewController.m
@@ -8,12 +8,34 @@
 
 #import "ConstraintsViewController.h"
 
+@interface ConstraintsViewController()
+- (void)addBarButton;
+- (void)updateConstraints:(id)sender;
+@end
+
 @implementation ConstraintsViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self addBarButton];
+}
+
+#pragma mark - Private Instance methods
+
+- (void)addBarButton
+{
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"Reorder"
+                                                             style:UIBarButtonItemStylePlain
+                                                            target:self
+                                                            action:@selector(updateConstraints:)];
+    self.navigationItem.rightBarButtonItem = item;
+}
+
+- (void)updateConstraints:(id)sender
+{
+
 }
 
 @end
