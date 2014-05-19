68b48fdff90c0a2968682c766f7f96db230e8c48
André Schneider <hello@andreschneider.me>
2014-05-19 22:59:32 +0200

    Add and layout views.

--- Popping/ConstraintsViewController.m
+++ Popping/ConstraintsViewController.m
@@ -9,7 +9,11 @@
 #import "ConstraintsViewController.h"
 
 @interface ConstraintsViewController()
+@property(nonatomic) UIView *redView;
+@property(nonatomic) UIView *greenView;
+@property(nonatomic) UIView *blueView;
 - (void)addBarButton;
+- (void)addViews;
 - (void)updateConstraints:(id)sender;
 @end
 
@@ -20,6 +24,8 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     [self addBarButton];
+    [self addViews];
+    [self updateConstraints:nil];
 }
 
 #pragma mark - Private Instance methods
@@ -33,9 +39,49 @@
     self.navigationItem.rightBarButtonItem = item;
 }
 
+- (void)addViews
+{
+    self.redView = [UIView new];
+    self.redView.backgroundColor = [UIColor redColor];
+    self.redView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.greenView = [UIView new];
+    self.greenView.backgroundColor = [UIColor greenColor];
+    self.greenView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.blueView = [UIView new];
+    self.blueView.backgroundColor = [UIColor blueColor];
+    self.blueView.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [self.view addSubview:self.redView];
+    [self.view addSubview:self.greenView];
+    [self.view addSubview:self.blueView];
+}
+
 - (void)updateConstraints:(id)sender
 {
+    NSDictionary *views = NSDictionaryOfVariableBindings(_redView, _greenView, _blueView);
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"H:|-[_redView]-|"
+                               options:0
+                               metrics:nil
+                               views:views]];
 
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"H:|-[_greenView]-[_blueView(==_greenView)]-|"
+                               options:NSLayoutFormatAlignAllTop
+                               metrics:nil
+                               views:views]];
+
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"V:|-(88)-[_redView]-[_greenView(==_redView)]-|"
+                               options:0
+                               metrics:nil
+                               views:views]];
+
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"V:|-(88)-[_redView]-[_blueView(==_redView)]-|"
+                               options:0
+                               metrics:nil
+                               views:views]];
 }
 
 @end
