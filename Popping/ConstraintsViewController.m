27ce2b2188e96224f4c8278715b0d75a7dfb3b41
André Schneider <hello@andreschneider.me>
2014-05-20 14:29:53 +0200

    Shuffle the views when updating the constraints.

--- Popping/ConstraintsViewController.m
+++ Popping/ConstraintsViewController.m
@@ -15,6 +15,7 @@
 - (void)addBarButton;
 - (void)addViews;
 - (void)updateConstraints:(id)sender;
+- (NSArray *)shuffledArrayFromArray:(NSArray *)array;
 @end
 
 @implementation ConstraintsViewController
@@ -44,12 +45,15 @@
     self.redView = [UIView new];
     self.redView.backgroundColor = [UIColor redColor];
     self.redView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.redView.layer.cornerRadius = 4.f;
     self.greenView = [UIView new];
     self.greenView.backgroundColor = [UIColor greenColor];
     self.greenView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.greenView.layer.cornerRadius = 4.f;
     self.blueView = [UIView new];
     self.blueView.backgroundColor = [UIColor blueColor];
     self.blueView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.blueView.layer.cornerRadius = 4.f;
 
     [self.view addSubview:self.redView];
     [self.view addSubview:self.greenView];
@@ -58,30 +62,67 @@
 
 - (void)updateConstraints:(id)sender
 {
+    [self.view layoutIfNeeded];
+    [self.view removeConstraints:self.view.constraints];
+
     NSDictionary *views = NSDictionaryOfVariableBindings(_redView, _greenView, _blueView);
+    NSArray *viewNames = [self shuffledArrayFromArray:views.allKeys];
+
+    NSString *firstViewKey= viewNames[0];
+    NSString *secondViewKey = viewNames[1];
+    NSString *thirdViewKey = viewNames[2];
+
+    NSString *horizontalFormat = [NSString stringWithFormat:@"H:|-[%@]-|", firstViewKey];
+    NSString *additionalHorizontalFormat = [NSString stringWithFormat:@"H:|-[%1$@]-[%2$@(==%1$@)]-|", secondViewKey, thirdViewKey];
+    NSString *verticalFormat = [NSString stringWithFormat:@"V:|-(88)-[%1$@]-[%2$@(==%1$@)]-|", firstViewKey, secondViewKey];
+    NSString *additionalVerticalFormat = [NSString stringWithFormat:@"V:|-(88)-[%1$@]-[%2$@(==%1$@)]-|", firstViewKey, thirdViewKey];
+
     [self.view addConstraints:[NSLayoutConstraint
-                               constraintsWithVisualFormat:@"H:|-[_redView]-|"
+                               constraintsWithVisualFormat:horizontalFormat
                                options:0
                                metrics:nil
                                views:views]];
 
     [self.view addConstraints:[NSLayoutConstraint
-                               constraintsWithVisualFormat:@"H:|-[_greenView]-[_blueView(==_greenView)]-|"
+                               constraintsWithVisualFormat:additionalHorizontalFormat
                                options:NSLayoutFormatAlignAllTop
                                metrics:nil
                                views:views]];
 
     [self.view addConstraints:[NSLayoutConstraint
-                               constraintsWithVisualFormat:@"V:|-(88)-[_redView]-[_greenView(==_redView)]-|"
+                               constraintsWithVisualFormat:verticalFormat
                                options:0
                                metrics:nil
                                views:views]];
 
     [self.view addConstraints:[NSLayoutConstraint
-                               constraintsWithVisualFormat:@"V:|-(88)-[_redView]-[_blueView(==_redView)]-|"
+                               constraintsWithVisualFormat:additionalVerticalFormat
                                options:0
                                metrics:nil
                                views:views]];
+
+    [UIView animateWithDuration:0.5
+                          delay:0
+         usingSpringWithDamping:0.7
+          initialSpringVelocity:0.7
+                        options:0
+                     animations:^{
+        [self.view layoutIfNeeded];
+    } completion:NULL];
+}
+
+- (NSArray *)shuffledArrayFromArray:(NSArray *)array
+{
+    NSMutableArray *shuffleArray = [array mutableCopy];
+    NSUInteger count = [shuffleArray count];
+
+    for (NSUInteger i = 0; i < count; ++i) {
+        NSUInteger nElements = count - i;
+        NSUInteger n = arc4random_uniform((uint32_t)nElements) + i;
+        [shuffleArray exchangeObjectAtIndex:i withObjectAtIndex:n];
+    }
+
+    return [shuffleArray copy];
 }
 
 @end
