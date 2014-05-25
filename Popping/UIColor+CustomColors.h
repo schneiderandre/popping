7f88e4a99743f8cf11e8b8b44a89f3d6bca5e3dd
André Schneider <hello@andreschneider.me>
2014-05-25 16:26:34 +0200

    Add some nice looking colors.

---
+++ Popping/UIColor+CustomColors.h
@@ -0,0 +1,18 @@
+//
+//  UIColor+CustomColors.h
+//  Popping
+//
+//  Created by André Schneider on 25.05.14.
+//  Copyright (c) 2014 André Schneider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (CustomColors)
+
++ (UIColor *)customGrayColor;
++ (UIColor *)customRedColor;
++ (UIColor *)customYellowColor;
++ (UIColor *)customGreenColor;
+
+@end
---
+++ Popping/UIColor+CustomColors.m
@@ -0,0 +1,45 @@
+//
+//  UIColor+CustomColors.m
+//  Popping
+//
+//  Created by André Schneider on 25.05.14.
+//  Copyright (c) 2014 André Schneider. All rights reserved.
+//
+
+#import "UIColor+CustomColors.h"
+
+@implementation UIColor (CustomColors)
+
++ (UIColor *)customGrayColor
+{
+    return [self colorWithRed:52 green:73 blue:94];
+}
+
++ (UIColor *)customRedColor
+{
+    return [self colorWithRed:231 green:76 blue:60];
+}
+
++ (UIColor *)customYellowColor
+{
+    return [self colorWithRed:241 green:196 blue:15];
+}
+
++ (UIColor *)customGreenColor
+{
+    return [self colorWithRed:46 green:204 blue:113];
+}
+
+#pragma mark - Private class methods
+
++ (UIColor *)colorWithRed:(NSUInteger)red
+                    green:(NSUInteger)green
+                     blue:(NSUInteger)blue
+{
+    return [UIColor colorWithRed:(float)(red/255.f)
+                           green:(float)(green/255.f)
+                            blue:(float)(blue/255.f)
+                           alpha:1.f];
+}
+
+@end
