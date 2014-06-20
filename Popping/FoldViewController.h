9df7a8caf8f4e524e9da68c428c699955728e22a
André Schneider <hello@andreschneider.me>
2014-06-20 19:43:37 +0200

    Add FoldViewController.

---
+++ Popping/FoldViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoldViewController.h
+//  Popping
+//
+//  Created by André Schneider on 20.06.14.
+//  Copyright (c) 2014 André Schneider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoldViewController : UIViewController
+
+@end
---
+++ Popping/FoldViewController.m
@@ -0,0 +1,49 @@
+//
+//  FoldViewController.m
+//  Popping
+//
+//  Created by André Schneider on 20.06.14.
+//  Copyright (c) 2014 André Schneider. All rights reserved.
+//
+
+#import "FoldViewController.h"
+
+@interface FoldViewController ()
+
+@end
+
+@implementation FoldViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
